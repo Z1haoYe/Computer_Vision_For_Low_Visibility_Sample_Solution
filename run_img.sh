0bf67268e78a2823b778fdb2004ee9d194fd3d29
Zihao Ye <ye277@purdue.edu>
2023-12-18 18:05:49 -0500

    run_img

--- run_img.sh
+++ run_img.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 # Set your specific data path and processed data directory
-DATA_TYPE="video"
+declare -r frame=10 #Extract every Nth frame
+DATA_TYPE="images"
 DATA_PATH="/home/zihao/Documents/cvlv/footage/single_building/vids.MP4"
+IMG_PATH="/home/zihao/Documents/cvlv/footage/single_building/img_d$frame"
 PROCESSED_DATA_DIR="/home/zihao/Documents/cvlv/nerf/single_building"
 NERFACTO_DATA_DIR="/home/zihao/Documents/cvlv/nerf/single_building/output"
+
+python3 ./util/Vid_to_img.py --video_path "$DATA_PATH" --output_path "$IMG_PATH" --fps_divider "$frame"
+
+# DEHAZE(WIP)
+# python3 ./util/YOLY/RW_dehazing.py
 
 # Run the following command if the error "QObject::moveToThread: Current thread (0x560bf022c870) is not the object's thread (0x560bf0243430). Cannot move to target thread (0x560bf022c870)..." occurs
 export QT_QPA_PLATFORM=offscreen
 
 # Run the command
 # ns-process-data {video,images,polycam,record3d} --data {DATA_PATH} --output-dir {PROCESSED_DATA_DIR}
-ns-process-data "$DATA_TYPE" --data "$DATA_PATH" --output-dir "$PROCESSED_DATA_DIR"
+ns-process-data "$DATA_TYPE" --data "$IMG_PATH" --output-dir "$PROCESSED_DATA_DIR"
 
 # ns-train nerfacto --data {PROCESSED_DATA_DIR}
 # ns-train nerfacto --data "$PROCESSED_DATA_DIR" --output-dir "$NERFACTO_DATA_DIR"
